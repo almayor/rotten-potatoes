0895042edd08cba7e288f47b47eeef30ed89b6bd
almayor <mayorovme@gmail.com>
2020-04-21 23:41:59 +0300

    Users can create reviews

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
@@ -11,11 +14,44 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+  	.lean()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
 // OUR MOCK ARRAY OF PROJECTS
 let reviews = [
   { title: "Great Review", movieTitle: "Batman II" },
-  { title: "Awesome Movie", movieTitle: "Titanic" }
+  { title: "Awesome Movie", movieTitle: "Titanic" },
+  { title: "Amazing", movieTitle: "Brokeback Mountain" }
 ]
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const Review = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String,
+});
+
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
