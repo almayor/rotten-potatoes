ede8b3bb92307b67b864230f095558a9c3de7be4
almayor <mayorovme@gmail.com>
2020-04-22 00:41:16 +0300

    Users can edit and update reviews

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -44,8 +45,9 @@ const Review = mongoose.model('Review', {
   movieTitle: String,
 });
 
+// NEW
 app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
+  res.render('reviews-new', {title: "New Review"});
 });
 
 app.post('/reviews', (req, res) => {
@@ -67,3 +69,29 @@ app.get('/reviews/:id', (req, res) => {
     console.log(err.message);
   })
 });
+
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+  Review.findById(req.params.id)
+  .lean()
+  .then((review) => {
+    res.render('reviews-edit', {review: review, title: "Edit Review"});
+  }).catch((err) => {
+    console.log(err.message);
+  })
+});
+
+// override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'));
+
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+  	.lean()
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
