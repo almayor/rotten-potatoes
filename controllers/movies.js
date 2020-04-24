5e4596e657df3ea1370521c127f89eba2997c9a3
almayor <mayorovme@gmail.com>
2020-04-24 20:15:47 +0300

    Users can see movie pages and trailers

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,15 +4,39 @@ const API_KEY = "d6d213e27bfb10d52d43288fb95ef788"
 
 module.exports = function (app) {
 
-    const MovieDb = require('moviedb-promise')
-    const moviedb = new MovieDb(API_KEY)
+	const MovieDb = require('moviedb-promise');
+	const moviedb = new MovieDb(API_KEY);
 
     app.get('/', (req, res) => {
-        moviedb.miscNowPlayingMovies()
-        .then(response => {
-            res.render('movies-index', { movies: response.results });
+        Promise.all([
+            moviedb.miscNowPlayingMovies(),
+            moviedb.genreMovieList(),
+        ])
+        .then(responses => {
+        	const movies = responses[0].results;
+        	const genreList = responses[1].genres;
+        	const genreMap = Object.fromEntries(
+        		genreList.map(genre => [genre.id, genre.name])
+        	);
+
+        	for (let i = 0; i < movies.length; i++)
+                movies[i].genre_names = movies[i].genre_ids.map(id => genreMap[id]);
+
+        	res.render('movies-index', { movies: movies });
         })
         .catch(console.error)
     })
 
+    app.get('/movies/:id', (req, res) => {
+        Promise.all([
+            moviedb.movieInfo({ id: req.params.id }),
+            moviedb.movieTrailers({ id: req.params.id }),
+        ])
+        .then(responses => {
+            const [movie, videos] = responses;
+            movie.trailer_youtube_id = videos.youtube[0].source;
+            res.render('movies-show', { movie: movie });
+        })
+        .catch(console.error)
+    })
 }
